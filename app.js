3453c0730783a6b8aabcb98ba52a0983d109f582
vaishnavi <vaishnavit582@gmail.com>
2024-07-26 12:28:53 +0530

    initial setup

---
+++ app.js
@@ -0,0 +1,16 @@
+const express=require("express");
+const app=express();
+const cookieParser=require("cookie-parser");
+const path=require("path");
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,"public")));
+app.set("view engine","ejs");
+app.get("/",(req,res)=>{
+    res.send("hey");
+})
+
+app.listen(3000,()=>{
+    console.log("server is running on port 3000");
+})
---
+++ models/user-model.js
@@ -0,0 +1,22 @@
+const mongoose=require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/Ecorm-Cart");
+
+const userSchema=mongoose.Schema({
+
+    fullname:String,
+    email:String,
+    password:String,
+    cart:{
+        type:Array,
+        default:[]
+    },
+    isAdmin:Boolean,
+    orders:{
+        type:Array,
+        default:[]
+    },
+    contact:Number,
+    picture:String
+})
+
+module.exports=mongoose.model("user",userSchema);
