a9bbc4c610cc39f58fde7a2866c5d9188b466213
vaishnavi <vaishnavit582@gmail.com>
2024-07-26 14:48:36 +0530

    models

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ const express=require("express");
 const app=express();
 const cookieParser=require("cookie-parser");
 const path=require("path");
+const ownersRouter=require("./routes/ownersRouter");
+const productRouter=require("./routes/productRouter");
+const usersRouter=require("./routes/usersRouter")
+const db=require("./config/mongoose-connection")
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,"public")));
 app.set("view engine","ejs");
-app.get("/",(req,res)=>{
-    res.send("hey");
-})
+
+app.use("/owners",ownersRouter);
+app.use("/users",usersRouter);
+app.use("/products",productRouter);
 
 app.listen(3000,()=>{
     console.log("server is running on port 3000");
---
+++ config/mongoose-connection.js
@@ -0,0 +1,8 @@
+const mongoose=require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/Ecorm-Cart").then(function (){
+    console.log("connected");
+}).catch((err)=>{
+    console.log(err);
+})
+
+module.exports=mongoose.connection;
