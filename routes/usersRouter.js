ec4430dafec8705c1b4b2369e96030e502238aea
vaishnavi <vaishnavit582@gmail.com>
2024-07-27 12:11:55 +0530

    Ecom Cart

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,44 +1,80 @@
-const express=require("express");
-const router=express.Router();
-const userModel=require("../models/user-model")
-const bcrypt=require("bcrypt")
-const jwt=require("jsonwebtoken");
-const {generateToken}=require("../utils/generateToken")
-router.get("/",function(req,res){
-   res.send("hey it is working");
-})
+// const express=require("express");
+// const router=express.Router();
+// const userModel=require("../models/user-model")
+// const bcrypt=require("bcrypt")
+// const jwt=require("jsonwebtoken");
+// const {generateToken}=require("../utils/generateToken")
+// router.get("/",function(req,res){
+//    res.send("hey it is working");
+// })
 
-router.post("/register", async function(req,res){
-  try{
-   let {email,password,fullname}=req.body;
-    bcrypt.genSalt(10,function(err,salt){
-      bcrypt.hash(password,salt,async function(err,hash){
-         if(err) return res.send(err.message);
-         else {
-            let user=await userModel.create({
-               email,
-               password:hash,
-               fullname,
+// router.post("/register", async function(req,res){
+//   try{
+//    let {email,password,fullname}=req.body;
+//     bcrypt.genSalt(10,function(err,salt){
+//       bcrypt.hash(password,salt,async function(err,hash){
+//          if(err) return res.send(err.message);
+//          else {
+//             let user=await userModel.create({
+//                email,
+//                password:hash,
+//                fullname,
 
                
                
-            });
-           let token=generateToken(user);
-           res.cookie("token",token);
-           res.send("user created succesfully");
+//             });
+//            let token=generateToken(user);
+//            res.cookie("token",token);
+//            res.send("user created succesfully");
 
-         }
+//          }
 
-      })
-    })
+//       })
+//     })
    
    
+//   }
+//   catch(err){
+//    console.log(err);
+//   }
+
+
+// })
+
+// module.exports=router;
+
+
+const express = require("express");
+const router = express.Router();
+const userModel = require("../models/user-model");
+const bcrypt = require("bcrypt");
+const { generateToken } = require("../utils/generateToken");
+
+router.get("/", function (req, res) {
+  res.send("hey it is working");
+});
+
+router.post("/register", async function (req, res) {
+  try {
+    let { email, password, fullname } = req.body;
+    bcrypt.genSalt(10, function (err, salt) {
+      bcrypt.hash(password, salt, async function (err, hash) {
+        if (err) return res.send(err.message);
+        else {
+          let user = await userModel.create({
+            email,
+            password: hash,
+            fullname,
+          });
+          let token = generateToken(user);
+          res.cookie("token", token);
+          res.send("user created successfully");
+        }
+      });
+    });
+  } catch (err) {
+    console.log(err);
   }
-  catch(err){
-   console.log(err);
-  }
+});
 
-
-})
-
-module.exports=router;
+module.exports = router;
--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,6 +1,7 @@
-const jwt=require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
-const generateToken=(user)=>{
-    return JsonWebTokenError.sign({email:user.email,id:user._id},process.env.JWT_KEY);
-}
-module.exports.generatetoken=generateToken;
+const generateToken = (user) => {
+  return jwt.sign({ email: user.email, id: user._id }, process.env.JWT_KEY);
+};
+
+module.exports = { generateToken };
