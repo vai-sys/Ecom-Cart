a6c9d22ea749df333e88b9a2877287596b6ded30
vaishnavi Thorat <122679661+vai-sys@users.noreply.github.com>
2024-07-29 21:45:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(flash());
-app.set("view engine", "ejs");
+
 
 app.use("/", indexRouter);
 app.use("/owners", ownersRouter);
